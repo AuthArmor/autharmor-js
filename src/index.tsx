0a458d1749148b8d5eeb2eb4b8542254c99317e9
saraelsa <sara-etx@outlook.com>
2023-05-19 15:30:00 -0400

    Create initial UI

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import { AuthenticationForm } from "./ui/AuthenticationForm";
+import { LogInForm } from "./ui/LogInForm";
+
+export { AuthenticationForm, LogInForm };
+
+export * from "./api";
+export * from "./client";
+export * from "./infrastructure";
+export * from "./webAuthn";
---
+++ src/ui/common/Dialog.tsx
@@ -0,0 +1,45 @@
+import { Portal, Show } from "solid-js/web";
+
+export interface IDialogProps {
+    title?: string;
+
+    statusMessage: string;
+    statusType: DialogStatusType;
+
+    alternateAction?: string;
+    onAlternateActionClick?: () => void;
+
+    onClose?: () => void;
+}
+
+export type DialogStatusType = "waiting" | "success" | "error";
+
+export function Dialog(props: IDialogProps) {
+    const handleClose = () => {
+        props.onClose?.();
+    }
+
+    const handleAlternateActionClick = () => {
+        props.onAlternateActionClick?.();
+    };
+
+    return (
+        <Portal>
+            <div>
+                <Show when={props.onClose !== undefined}>
+                    <button onClick={handleClose}>Close</button>
+                </Show>
+
+                <Show when={props.title !== undefined}>
+                    <p>{props.title}</p>
+                </Show>
+
+                <div>{props.statusMessage}</div>
+
+                <Show when={props.onAlternateActionClick !== undefined}>
+                    <button onClick={handleAlternateActionClick}>{props.alternateAction}</button>
+                </Show>
+            </div>
+        </Portal>
+    );
+}
