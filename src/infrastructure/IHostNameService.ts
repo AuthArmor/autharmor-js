efbadbe14b74d30a9a3dfb36d5c2cf9a3c35bb23
saraelsa <sara-etx@outlook.com>
2023-05-19 15:21:18 -0400

    Fix request signer incorrectly using callee host instead of caller host

---
+++ src/infrastructure/IHostNameService.ts
@@ -0,0 +1,11 @@
+/**
+ * Interface for retrieving the current hostname.
+ */
+export interface IHostNameService {
+    /**
+     * Gets the current hostname.
+     * 
+     * @returns The current hostname.
+     */
+    getHostName: () => string;
+}
