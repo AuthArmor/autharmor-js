6e58bd2fa83498d80c0656d43e8607d700285253
Sara Elsayeda <swe@saraelsa.net>
2023-08-07 08:11:04 -0400

    Remove overriding message property from ApiError

--- src/api/errors/ApiError.ts
+++ src/api/errors/ApiError.ts
@@ -2,14 +2,12 @@ import { IApiError } from "../models";
 
 export class ApiError extends Error {
     public readonly statusCode: number;
-    public readonly message: string;
     public readonly trackingId: string;
 
     public constructor(public readonly rawError: IApiError) {
         super(`[${rawError.error_code}] ${rawError.error_message}`);
 
         this.statusCode = rawError.error_code;
-        this.message = rawError.error_message;
         this.trackingId = rawError.tracking_id;
     }
 }
