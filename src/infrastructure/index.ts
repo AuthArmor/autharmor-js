625e8ce0149f672afadbf3916b2961d1a87711bc
Sara Elsayeda <swe@saraelsa.net>
2023-08-16 18:16:08 -0400

    Fix stray export

--- src/infrastructure/index.ts
+++ src/infrastructure/index.ts
@@ -1,22 +1,9 @@
-import { IBase64Coder } from "./IBase64Coder";
-import { INonceGenerator } from "./INonceGenerator";
-import { IReCaptchaService } from "./IReCaptchaService";
-import { ISha256StringHasher } from "./ISha256StringHasher";
-import { ISystemClock } from "./ISystemClock";
-import { BrowserBase64Coder } from "./BrowserBase64Coder";
-import { BrowserNonceGenerator } from "./BrowserNonceGenerator";
-import { BlankReCaptchaService } from "./BlankReCaptchaService";
-import { GoogleReCaptchaService } from "./GoogleReCaptchaService";
-import { BrowserSha256StringHasher } from "./BrowserSha256StringHasher";
-import { NativeSystemClock } from "./NativeSystemClock";
-
-export type { IBase64Coder, INonceGenerator, IReCaptchaService, ISha256StringHasher, ISystemClock };
-
-export {
-    BrowserBase64Coder,
-    BrowserNonceGenerator,
-    BlankReCaptchaService,
-    GoogleReCaptchaService,
-    BrowserSha256StringHasher,
-    NativeSystemClock
-};
+export * from "./IBase64Coder";
+export * from "./INonceGenerator";
+export * from "./IReCaptchaService";
+export * from "./ISha256StringHasher";
+export * from "./ISystemClock";
+export * from "./BrowserBase64Coder";
+export * from "./BrowserNonceGenerator";
+export * from "./BrowserSha256StringHasher";
+export * from "./NativeSystemClock";
